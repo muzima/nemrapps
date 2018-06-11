7b1d89a85a6d93a7a22a35c340e14dd05c33ceb6
Willa Mhawila <a.mhawila@gmail.com>
2018-06-11 11:47:20 +0300

    NEMR-38: Added a class to hold uuid constants for person attributes

---
+++ omod/src/main/java/org/openmrs/module/nemrapps/NemrConstants.java
@@ -0,0 +1,13 @@
+package org.openmrs.module.nemrapps;
+
+/**
+ * Created by Willa aka Baba Imu on 6/11/18.
+ */
+public class NemrConstants {
+    // TODO: Make this a global property or something else easily configurable.
+    public static final String ALTERNATIVE_PHONES_UUID = "4b01cce2-a56b-44ad-b6dd-68cc31127e93";
+
+    public static final String NEIGHBOR_NAME_UUID = "12048766-d478-4f31-b9e1-88630198b1c1";
+
+    public static final String NEIGHBOR_PHONE_NUMBER_UUID = "57963f24-9127-499f-86fe-dd8b87110b19";
+}
--- omod/src/main/java/org/openmrs/module/nemrapps/fragment/controller/patientdashboard/ContactInfoInlineFragmentController.java
+++ omod/src/main/java/org/openmrs/module/nemrapps/fragment/controller/patientdashboard/ContactInfoInlineFragmentController.java
@@ -6,18 +6,11 @@ import org.openmrs.PersonAttributeType;
 import org.openmrs.api.PersonService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.emrapi.patient.PatientDomainWrapper;
+import org.openmrs.module.nemrapps.NemrConstants;
 import org.openmrs.ui.framework.annotation.InjectBeans;
 import org.openmrs.ui.framework.fragment.FragmentConfiguration;
 
 public class ContactInfoInlineFragmentController extends org.openmrs.module.coreapps.fragment.controller.patientdashboard.ContactInfoInlineFragmentController {
-	
-	// TODO: Make this a global property or something else easily configurable.
-	private static final String ALTERNATIVE_PHONES_UUID = "4b01cce2-a56b-44ad-b6dd-68cc31127e93";
-	
-	private static final String NEIGHBOR_NAME_UUID = "12048766-d478-4f31-b9e1-88630198b1c1";
-	
-	private static final String NEIGHBOR_PHONE_NUMBER_UUID = "57963f24-9127-499f-86fe-dd8b87110b19";
-	
 	public void controller(FragmentConfiguration config, @InjectBeans PatientDomainWrapper wrapper) {
 		
 		super.controller(config, wrapper);
@@ -26,9 +19,9 @@ public class ContactInfoInlineFragmentController extends org.openmrs.module.core
 		
 		// Get the alternative phone numbers.
 		Person person = ((PatientDomainWrapper) config.get("patient")).getPatient().getPerson();
-		PersonAttributeType altPhonesType = personService.getPersonAttributeTypeByUuid(ALTERNATIVE_PHONES_UUID);
-		PersonAttributeType neighborNameType = personService.getPersonAttributeTypeByUuid(NEIGHBOR_NAME_UUID);
-		PersonAttributeType neighborPhoneNumberType = personService.getPersonAttributeTypeByUuid(NEIGHBOR_PHONE_NUMBER_UUID);
+		PersonAttributeType altPhonesType = personService.getPersonAttributeTypeByUuid(NemrConstants.ALTERNATIVE_PHONES_UUID);
+		PersonAttributeType neighborNameType = personService.getPersonAttributeTypeByUuid(NemrConstants.NEIGHBOR_NAME_UUID);
+		PersonAttributeType neighborPhoneNumberType = personService.getPersonAttributeTypeByUuid(NemrConstants.NEIGHBOR_PHONE_NUMBER_UUID);
 		
 		PersonAttribute altPhones = altPhonesType == null ? null : person.getAttribute(altPhonesType);
 		PersonAttribute neighborName = neighborNameType == null ? null : person.getAttribute(neighborNameType);
